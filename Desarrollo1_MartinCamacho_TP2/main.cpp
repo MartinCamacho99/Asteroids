c7c288ce0ac497c2a60b9837dc2fd669f6390c99
MartinCamacho99 <113991248+MartinCamacho99@users.noreply.github.com>
2022-09-27 02:26:32 -0300

    movimiento de nave y comienzo de disparos

--- Desarrollo1_MartinCamacho_TP2/main.cpp
+++ Desarrollo1_MartinCamacho_TP2/main.cpp
@@ -8,9 +8,18 @@ using namespace std;
 int main()
 {
     // Inicialización
-    InitWindow(1280, 720, "Asteroid");
+    InitWindow(1920, 0, "Asteroid");
 
-   
+    struct Bullet
+    {
+        float centerX;
+        float centerY;
+        float radius;
+        Color color;
+        bool bulletActive;
+    };
+  
+
     Rectangle ship;
     ship.x = 320;
     ship.y = 240;
@@ -26,12 +35,23 @@ int main()
     Vector2 shipPos;
     Vector2 dir;
     Vector2 dirNormalized;
+    Vector2 dirNormalizedbul;
+    Vector2 shipAce;
+    shipAce.x = 1;
+    shipAce.y = 1;
+
+    Bullet bullet[100];
+    bullet[1].centerX = 100;
+    bullet[1].centerY = 100;
+    bullet[1].radius = 1;
+    bullet[1].color = YELLOW;
+    bullet[1].bulletActive = false;
+
     // Loop
 
     while (!WindowShouldClose())
     {
         // Chequeo de Input
-        //a
         mousePos.x = GetMouseX();
         mousePos.y = GetMouseY();
         shipPos.x = ship.x + origin.x;
@@ -40,13 +60,37 @@ int main()
         dir.y = mousePos.y - shipPos.y;
         rotation = atan(dir.y / dir.x)* 180 / PI;
 
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        {
+            dirNormalized = Vector2Normalize(dir);
+            shipAce.x += dirNormalized.x*0.1;
+            shipAce.y += dirNormalized.y * 0.1;
+        }
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            bullet[1].bulletActive = true;
+            dirNormalizedbul = Vector2Normalize(dir);
+
+            bullet[1].centerX += dirNormalizedbul.x*0.01;
+            bullet[1].centerY += dirNormalizedbul.y * 0.01;
+
+        }
+     
+
+        ship.x = ship.x + shipAce.x * GetFrameTime();
+        ship.y = ship.y + shipAce.y * GetFrameTime();
+        
+
         // Actualización
 
         // Dibujado
 
         BeginDrawing();
-        ClearBackground(WHITE); 
-        DrawRectanglePro(ship,origin, rotation,RED);
+        ClearBackground(BLACK); 
+        DrawRectanglePro(ship,origin, rotation,RED);       
+        DrawCircle(bullet[1].centerX, bullet[1].centerY, bullet[1].radius, bullet[1].color);
+
 
         EndDrawing();
     }
